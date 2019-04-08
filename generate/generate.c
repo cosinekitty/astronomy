f163d86170d4b4ea9b28a910e5de593ffd5777f7
Don Cross <cosinekitty@gmail.com>
2019-04-08 08:57:43 -0400

    Starting to work on astronomy source generator C program.

---
+++ generate/generate.c
@@ -0,0 +1,56 @@
+/*
+    MIT License
+
+    Copyright (c) 2019 Don Cross <cosinekitty@gmail.com>
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+static int PrintUsage();
+static int GenerateAllSource();
+
+int main(int argc, const char *argv[])
+{
+    if (argc == 2 && !strcmp(argv[1], "all"))
+        return GenerateAllSource();
+
+    return PrintUsage();
+}
+
+static int PrintUsage()
+{
+    fprintf(stderr, 
+        "\n"
+        "USAGE:\n"
+        "\n"
+        "generate all\n"
+        "    Generate astronomy source code for all target languages.\n"
+        "\n"
+    );
+
+    return 1;
+}
+
+static int GenerateAllSource()
+{
+    return 1;    /* not yet implemented */
+}
