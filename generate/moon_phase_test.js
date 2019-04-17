d00e40341b9e6869f51d9d7739f27625d26b50c8
Don Cross <cosinekitty@gmail.com>
2019-04-17 05:03:47 -0400

    Added test of ecliptic longitude formula.
    Using moon phase test data, exercised the calculation of
    ecliptic longitude of the Sun and Moon at indicated times.
    Compared the actual difference in longitude with 90*phase degrees,
    where phase is 0 for new moon, 1 for first quarter, 2 for full,
    3 for third quarter. The results always agree within 1 arcminute
    (just barely).
    
    Added this to the unit test.
    
    Will come back and add unit tests for the phase searcher once
    I implement it.

--- generate/moon_phase_test.js
+++ generate/moon_phase_test.js
@@ -1,23 +1,48 @@
 const fs = require('fs');
 const Astronomy = require('../source/js/astronomy.js');
 
-function TestMoonPhases() {
-    const text = fs.readFileSync('moonphase/moonphases.txt', {encoding:'utf8'});
+function LoadMoonPhaseData(filename) {
+    // Load known moon phase times from US Naval Observatory.
+    const text = fs.readFileSync(filename, {encoding:'utf8'});
     const lines = text.trimRight().split('\n');
+    let data = [];
     for (let row of lines) {        
         let token = row.split(' ');
-        let phase = parseInt(token[0]);
-        let date = new Date(token[1]);
-        let gm = Astronomy.GeoVector('Moon', date);
+        data.push({phase:parseInt(token[0]), date:new Date(token[1])});
+    }
+    return data;
+}
+
+function TestLongitudes(data) {
+    // Using known moon phase times from US Naval Obs
+    let max_arcmin = 0;
+    for (let row of data) {
+        let gm = Astronomy.GeoVector('Moon', row.date);
         const moon_eclip = Astronomy.Ecliptic(gm.x, gm.y, gm.z);
 
-        let sun = Astronomy.GeoVector('Sun', date);
+        let sun = Astronomy.GeoVector('Sun', row.date);
         const sun_eclip = Astronomy.Ecliptic(sun.x, sun.y, sun.z);
 
         let elong = moon_eclip.elon - sun_eclip.elon;
         if (elong < 0) elong += 360;
-        console.log(`${phase} ${date.toISOString()} ${elong}`);
+        let expected_elong = 90 * row.phase;
+        let degree_error = Math.abs(elong - expected_elong);
+        if (degree_error > 180) degree_error = 360 - degree_error;
+        let arcmin = 60 * degree_error;
+        max_arcmin = Math.max(max_arcmin, arcmin);
+        //console.log(`${row.phase} ${row.date.toISOString()} ${elong} ${arcmin}`);
     }
+    console.log(`TestLongitudes: nrows = ${data.length}, max_arcmin = ${max_arcmin}`);
+    if (max_arcmin > 1.0) {
+        console.log(`TestLongitudes: EXCESSIVE ANGULAR ERROR`);
+        return 1;
+    }
+    return 0;
 }
 
-TestMoonPhases();
+function TestSearch(data) {
+    return 0;
+}
+
+const TestData = LoadMoonPhaseData('moonphase/moonphases.txt')
+process.exit(TestLongitudes(TestData) || TestSearch(TestData));
