0910df7c12b165ba1993f285ff1ee51298b562a6
Don Cross <cosinekitty@gmail.com>
2019-04-23 21:12:02 -0400

    Added seasons_test to Windows run.bat

--- generate/run.bat
+++ generate/run.bat
@@ -104,6 +104,11 @@ if errorlevel 1 (exit /b 1)
 if errorlevel 1 (exit /b 1)
 
 echo.
+echo Running seasons test.
+node seasons_test.js
+if errorlevel 1 (exit /b 1)
+
+echo.
 echo Running rise/set test.
 node rise_set_test.js
 if errorlevel 1 (exit /b 1)
