486aab5039760d42bf55cc085e0f823e2fbf04bb
Don Cross <cosinekitty@gmail.com>
2019-05-05 11:10:52 -0400

    Script to confirm build is clean.

---
+++ generate/verify_clean.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set clean=true
+for /f %%x in ('git status --porcelain') do (
+    set clean=false
+)
+
+if !clean! == false (
+    echo.verify_clean: There are local file changes - build is not clean:
+    git status
+    exit /b 1
+)
+
+echo.verify_clean: OK
+exit /b 0
