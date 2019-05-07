899466bcac6b3e72135fcd142d07e19515293a73
Don Cross <cosinekitty@gmail.com>
2019-05-07 16:18:28 -0400

    Updated Windows makedoc.bat to match Linux makedoc bash script.

--- generate/makedoc.bat
+++ generate/makedoc.bat
@@ -1,7 +1,7 @@
 @echo off
 echo.
-echo.Making documentation files.
-call jsdoc2md --template ../jsdoc2md/js.hbs --files ..\source\js\astronomy.js > ..\source\js\README.md
+echo.Making documentation files in Markdown format.
+call jsdoc2md --separators --template ../jsdoc2md/js.hbs --files ..\source\js\astronomy.js > ..\source\js\README.md
 if errorlevel 1 (
     echo.FATAL: error in jsdoc2md
     exit /b 1
@@ -11,6 +11,15 @@ node eol_hack.js ..\source\js\README.md
 if errorlevel 1 (
     echo.FATAL: error in eol_hack.js
     exit /b 1
+)
+
+echo.Making documentation in HTML format for local viewing.
+if exist html (
+    rd /s/q html
+)
+call jsdoc ../source/js/astronomy.js --destination html
+if errorlevel 1 (
+    echo.FATAL: error in jsdoc
 )
 
 exit /b 0
