09fc1a5d4d9fd37991e5186e374c5dbb361d9ea2
Don Cross <cosinekitty@gmail.com>
2019-05-21 15:21:18 -0400

    Updated comments.

--- source/c/astronomy.c
+++ source/c/astronomy.c
@@ -3503,6 +3503,5 @@ astro_search_result_t Astronomy_SearchRelativeLongitude(astro_body_t body, doubl
     X   SearchLunarApsis
     -   SearchMaxElongation
     X   SearchPeakMagnitude
-    -   SearchRelativeLongitude
     X   SearchRiseSet
 */
