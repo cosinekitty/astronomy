5db8a914040a2987f0501e67140bb7038e9f41d8
Don Cross <cosinekitty@gmail.com>
2019-10-09 20:28:46 -0400

    Verifying that I can build dotnet core 2.2 programs in Travis CI.

---
+++ generate/dotnet/csharp_test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace csharp_test
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            return 0;
+        }
+    }
+}
