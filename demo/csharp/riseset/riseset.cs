96cd7e4c7ee16d4ff935901eddee61977c239174
Don Cross <cosinekitty@gmail.com>
2019-12-23 13:30:13 -0500

    C# demo: added riseset.

--- demo/csharp/riseset/riseset.cs
+++ demo/csharp/riseset/riseset.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using CosineKitty;
+using demo_helper;
 
 namespace riseset
 {
     class Program
     {
-        static void Main(string[] args)
+        static int PrintEvent(string name, AstroTime time)
         {
-            Console.WriteLine("Hello World!");
+            if (time == null)
+            {
+                Console.WriteLine("ERROR: Search failed for {0}", name);
+                return 1;
+            }
+            Console.WriteLine("{0,-8} : {1}", name, time);
+            return 0;
+        }
+
+        static int Main(string[] args)
+        {
+            Observer observer;
+            AstroTime time;
+            DemoHelper.ParseArgs("riseset", args, out observer, out time);
+            Console.WriteLine("search   : {0}", time);
+            if (0 != PrintEvent("sunrise",  Astronomy.SearchRiseSet(Body.Sun,  observer, Direction.Rise, time, 300.0))) return 1;
+            if (0 != PrintEvent("sunset",   Astronomy.SearchRiseSet(Body.Sun,  observer, Direction.Set,  time, 300.0))) return 1;
+            if (0 != PrintEvent("moonrise", Astronomy.SearchRiseSet(Body.Moon, observer, Direction.Rise, time, 300.0))) return 1;
+            if (0 != PrintEvent("moonset",  Astronomy.SearchRiseSet(Body.Moon, observer, Direction.Set,  time, 300.0))) return 1;
+            return 0;
         }
     }
 }
