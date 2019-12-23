ff75e707536560924abf9cb3625f9b6c17388178
Don Cross <cosinekitty@gmail.com>
2019-12-23 13:49:09 -0500

    C# demo: added seasons demo.

--- demo/csharp/seasons/seasons.cs
+++ demo/csharp/seasons/seasons.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using CosineKitty;
+using demo_helper;
 
 namespace seasons
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int year;
+            if (args.Length != 1 || !int.TryParse(args[0], out year) || year < Astronomy.MinYear || year > Astronomy.MaxYear)
+            {
+                Console.WriteLine("ERROR: Must provide year {0}..{1} on command line.", Astronomy.MinYear, Astronomy.MaxYear);
+                return 1;
+            }
+            SeasonsInfo seasons = Astronomy.Seasons(year);
+            Console.WriteLine("March equinox     : {0}", seasons.mar_equinox);
+            Console.WriteLine("June solstice     : {0}", seasons.jun_solstice);
+            Console.WriteLine("September equinox : {0}", seasons.sep_equinox);
+            Console.WriteLine("December solstice : {0}", seasons.dec_solstice);
+            return 0;
         }
     }
 }
