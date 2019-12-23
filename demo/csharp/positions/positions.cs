ff964879e4c304379a03b107c65cd8efa08debe8
Don Cross <cosinekitty@gmail.com>
2019-12-23 12:55:17 -0500

    C# demo: Added positions demo.

--- demo/csharp/positions/positions.cs
+++ demo/csharp/positions/positions.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using CosineKitty;
+using demo_helper;
 
 namespace positions
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var bodies = new Body[]
+            {
+                Body.Sun, Body.Moon, Body.Mercury, Body.Venus, Body.Mars,
+                Body.Jupiter, Body.Saturn, Body.Uranus, Body.Neptune, Body.Pluto
+            };
+
+            Observer observer;
+            AstroTime time;
+            DemoHelper.ParseArgs("positions", args, out observer, out time);
+            Console.WriteLine("UTC date = {0}", time);
+            Console.WriteLine();
+            Console.WriteLine("BODY           RA      DEC       AZ      ALT");
+            foreach (Body body in bodies)
+            {
+                Equatorial equ_2000 = Astronomy.Equator(body, time, observer, EquatorEpoch.J2000, Aberration.Corrected);
+                Equatorial equ_ofdate = Astronomy.Equator(body, time, observer, EquatorEpoch.OfDate, Aberration.Corrected);
+                Topocentric hor = Astronomy.Horizon(time, observer, equ_ofdate.ra, equ_ofdate.dec, Refraction.Normal);
+                Console.WriteLine("{0,-8} {1,8:0.00} {2,8:0.00} {3,8:0.00} {4,8:0.00}", body, equ_2000.ra, equ_2000.dec, hor.azimuth, hor.altitude);
+            }
+
+            return 0;
         }
     }
 }
