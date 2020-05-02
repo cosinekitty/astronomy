c28722120ea62f56e3c0c8c6fecda5e61ed25890
Don Cross <cosinekitty@gmail.com>
2020-05-02 17:01:21 -0400

    I decided to skip just the 25 constellation disagreements with HYG Database.

--- generate/make_constellation_data.py
+++ generate/make_constellation_data.py
@@ -9,6 +9,15 @@ import sys
 import re
 
 def Translate(inFileName, outFileName):
+    # Ignore 25 disagreements with hygdata_v3.
+    # See: https://github.com/astronexus/HYG-Database/issues/21
+    ignore = set([
+        3865, 7751, 10342, 19173, 22400, 27992, 29366,
+        30112, 52886, 58221, 59234, 67599, 72017, 74021,
+        84413, 85957, 87694, 89105, 91382, 92907, 93308,
+        95468, 100777, 101541, 105623
+    ])
+
     count = 0
     with open(inFileName, 'rt') as infile:
         with open(outFileName, 'wt') as outfile:
@@ -29,9 +38,7 @@ def Translate(inFileName, outFileName):
                             print('make_constellation_data: bad symbol "{}" in {} line {}'.format(sym, inFileName, lnum))
                             return 1
 
-                        # Without a magnitude cutoff, I get 25 disagreements with hygdata_v3.
-                        # See: https://github.com/astronexus/HYG-Database/issues/21
-                        if mag < 4.890:
+                        if id not in ignore:
                             count += 1
                             outfile.write('{:6d} {:10.6f} {:10.6f} {:3s} {:7.3f}\n'.format(id, ra, dec, sym, mag))
     print('make_constellation_data: Wrote {:d} test cases.'.format(count))
