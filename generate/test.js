e642000cf46914a46c1da21b8af7563e148d2eca
Don Cross <cosinekitty@gmail.com>
2020-06-03 09:54:49 -0400

    Merged constellation into test.js

--- generate/test.js
+++ generate/test.js
@@ -1266,6 +1266,38 @@ function Magnitude() {
 }
 
 
+function Constellation() {
+    const filename = 'constellation/test_input.txt';
+    const text = fs.readFileSync(filename, {encoding:'utf8'});
+    const lines = text.trimRight().split('\n');
+    let lnum = 0;
+    let failcount = 0;
+    for (let row of lines) {
+        ++lnum;
+        let token = row.trim().split(/\s+/);
+        if (token.length !== 4) {
+            Fail(`Bad data in ${filename} line ${lnum}: found ${token.length} tokens.`);
+        }
+        const id = parseInt(token[0]);
+        const ra = parseFloat(token[1]);
+        const dec = parseFloat(token[2]);
+        const symbol = token[3];
+        if (!/^[A-Z][A-Za-z]{2}$/.test(symbol)) {
+            Fail(`Invalid symbol "${symbol}" in ${filename} line ${lnum}`);
+        }
+        const constel = Astronomy.Constellation(ra, dec);
+        if (constel.symbol !== symbol) {
+            ++failcount;
+            console.error(`Star ${id}: expected ${symbol}, found ${constel.symbol} at B1875 RA=${constel.ra1875}, DEC=${constel.dec1875}`);
+        }
+    }
+    if (failcount > 0) {
+        Fail(`JS Constellation: ${failcount} failures.`);
+    }
+    console.log(`JS Constellation: PASS (verified ${lines.length})`);
+    return 0;
+}
+
 
 function main() {
     let args = process.argv.slice(2);
@@ -1277,6 +1309,7 @@ function main() {
     if (args.length === 1) {
         switch (args[0]) {
             case 'astro_check':     return AstroCheck();
+            case 'constellation':   return Constellation();
             case 'elongation':      return Elongation();
             case 'lunar_apsis':     return LunarApsis();
             case 'lunar_eclipse':   return LunarEclipse();
