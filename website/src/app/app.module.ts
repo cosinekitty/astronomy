22acc9fe9cf7972095e61e53fd000f7107283001
Mateo Tibaquira <nestormateo@gmail.com>
2021-02-02 22:46:48 -0500

    docs: ng add @angular/material @angular/flex-layout

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -1,6 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,8 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
