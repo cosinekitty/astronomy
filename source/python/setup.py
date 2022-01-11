ab9b5a5ce5d95a8c8c9315eab44e1b3f76eb78a6
Don Cross <cosinekitty@gmail.com>
2022-01-10 20:34:35 -0500

    pip, npm packages 2.0.10.
    The pip and npm astronomy-engine packages will have
    matching version numbers from now on, starting with
    2.0.10.
    
    https://pypi.org/project/astronomy-engine/2.0.10/
    https://www.npmjs.com/package/astronomy-engine

--- source/python/setup.py
+++ source/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='astronomy-engine',
-    version='1.0.1',
+    version='2.0.10',
     description='Astronomy calculation for Sun, Moon, and planets.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -13,6 +13,6 @@ setup(
     classifiers=[
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.7"
     ]
 )
