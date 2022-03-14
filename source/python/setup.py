1a645fea18a3fe042b90970151bea08b9d6cdace
Don Cross <cosinekitty@gmail.com>
2022-03-14 05:09:56 -0400

    Bumped npm,pypi versions to 2.0.13 to test publish.
    The way I was publishing the Python package was annoying.
    I found a better way to authenticate myself to pypi.org,
    but I needed to test it. This required bumping the version
    numbers of the packages. There is no difference between
    2.0.12 and 2.0.13.

--- source/python/setup.py
+++ source/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='astronomy-engine',
-    version='2.0.12',
+    version='2.0.13',
     description='Astronomy calculation for Sun, Moon, and planets.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
