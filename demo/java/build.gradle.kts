c318b586eb035463c85519697208c6be63609709
Ebrahim Byagowi <ebrahim@gnu.org>
2022-03-21 23:06:54 +0330

    kotlin: Add a Java demo project

---
+++ demo/java/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    application
+}
+
+group = "io.github.cosinekitty.astronomy.demo"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation("com.github.ebraminio:astronomy:2bcbf09edd335a42e013135e04c1418d0bdcde32")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+application {
+    mainClass.set("io.github.cosinekitty.astronomy.demo.Main")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
