ae5e744e1b9b7630fc4bf0c5f5cf5e0af6aa6e41
Don Cross <cosinekitty@gmail.com>
2022-03-22 12:55:49 -0400

    Verify that Java demos work. Prep more demos.
    Restructured the Java code so we pass in command
    line arguments to select which demo we want to run.
    We will also pass in date/time, latitude/longitude,
    or whatever numeric data we need for future demos.
    
    Automated test run of the Java demos from the
    unit test suite.

--- demo/java/src/main/java/io/github/cosinekitty/astronomy/demo/Main.java
+++ demo/java/src/main/java/io/github/cosinekitty/astronomy/demo/Main.java
@@ -1,11 +1,33 @@
 package io.github.cosinekitty.astronomy.demo;
 
 import java.util.Date;
-
 import io.github.cosinekitty.astronomy.AstroTime;
 
 public class Main {
+    private static String UsageText = String.join(System.getProperty("line.separator"),
+        "Command line arguments:",
+        "",
+        "    now",
+        "    Display current date and time using AstroTime.",
+        ""
+    );
+
     public static void main(String[] args) {
-        System.out.println(new AstroTime(new Date()));
+        int rc = 1;
+        if (args.length == 0) {
+            System.out.println(UsageText);
+        } else {
+            switch (args[0]) {
+                case "now":
+                    System.out.println(new AstroTime(new Date()));
+                    rc = 0;
+                    break;
+
+                default:
+                    System.out.println("ERROR: Unknown command line argument");
+                    break;
+            }
+        }
+        System.exit(rc);
     }
 }
