ad439c6d97338d1ea18b697bdd3325188c66b292
Don Cross <cosinekitty@gmail.com>
2022-04-14 13:16:09 -0400

    Kotlin Native work in progress: build errors.
    Checking in with the Kotlin Native tests turned off,
    because the code does not build yet. Seeking help.

--- demo/kotlin/native/moonphase.kt
+++ demo/kotlin/native/moonphase.kt
@@ -1,6 +1,8 @@
 import kotlin.system.exitProcess
+import io.github.cosinekitty.astronomy
 
 fun main() {
-    println("Moon Phase demo")
+    val time = Time(2019, 6, 15, 9, 15, 32.987)
+    println(time)
     exitProcess(0)
 }
