896df1ad19653138ceba80acb44c76f377a5d23c
Don Cross <cosinekitty@gmail.com>
2022-04-15 13:04:52 -0400

    Removed Kotlin Native tests for now.
    We are having difficulty getting Kotlin code to build for
    both JVM and Native. For now, the priority is to support JVM,
    so I am turning off installation of the Kotlin Native compiler.

--- demo/kotlin/README.md
+++ demo/kotlin/README.md
@@ -1,5 +1,4 @@
 # Kotlin demos
 
-### Moon Phase
-[`moonphase.kt`](native/moonphase.kt)
-A Kotlin Native app that displays the next 10 lunar quarter phases.
+(Nothing here yet. Stay tuned.)
+
--- demo/kotlin/native/moonphase.kt
+++
@@ -1,8 +0,0 @@
-import kotlin.system.exitProcess
-import io.github.cosinekitty.astronomy
-
-fun main() {
-    val time = Time(2019, 6, 15, 9, 15, 32.987)
-    println(time)
-    exitProcess(0)
-}
