9257b937f174870f0bac575a9fd7d69347849806
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-04-30 13:53:48 +0430

    Add TerseVector

--- source/dart/example/astronomy_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:astronomy/astronomy.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- source/dart/test/astronomy_test.dart
+++ source/dart/test/astronomy_test.dart
@@ -2,15 +2,13 @@ import 'package:astronomy/astronomy.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+  test('TerseVector methods', () {
+    final ones = TerseVector(x: 1.0, y: 1.0, z: 1.0);
+    expect(ones, ones + TerseVector.zero);
+    expect(ones, ones + TerseVector.zero);
+    expect(TerseVector(x: 6.0, y: 8.0, z: 4.0), TerseVector(x: 3.0, y: 4.0, z: 2.0) * 2.0);
+    expect(TerseVector(x: -1.5, y: 2.0, z: -1.0), TerseVector(x: -3.0, y: 4.0, z: -2.0) / 2.0);
+    expect(29.0, TerseVector(x: -3.0, y: 4.0, z: -2.0).quadrature());
+    expect(5.744562646538029, TerseVector(x: -2.0, y: -2.0, z: 5.0).magnitude());
   });
 }
