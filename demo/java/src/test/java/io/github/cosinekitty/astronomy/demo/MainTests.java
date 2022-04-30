13f6e9886661e4a4b7dc8a31649ad01892001b1c
Ebrahim Byagowi <ebrahim@gnu.org>
2022-04-30 11:44:46 +0430

    Kotlin: Use Instant.parse instead a custom date parser
    Also fixes the demo which had the same code.

--- demo/java/src/test/java/io/github/cosinekitty/astronomy/demo/MainTests.java
+++ demo/java/src/test/java/io/github/cosinekitty/astronomy/demo/MainTests.java
@@ -4,7 +4,6 @@ import io.github.cosinekitty.astronomy.Time;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
-import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,6 +11,6 @@ public class MainTests {
     @Test
     void main() {
         String time = "2022-01-01T12:00:00.000Z";
-        assertEquals(time, new Time(Date.from(Instant.parse(time))).toString());
+        assertEquals(time, Time.fromMillisecondsSince1970(Instant.parse(time).toEpochMilli()).toString());
     }
 }
