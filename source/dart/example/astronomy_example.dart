7f71c40d7bb8aecf196146aed18191b2f86957c2
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-04-30 07:19:46 +0430

    Add dart project

---
+++ source/dart/example/astronomy_example.dart
@@ -0,0 +1,6 @@
+import 'package:astronomy/astronomy.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ source/dart/lib/astronomy.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library astronomy;
+
+export 'src/astronomy_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
