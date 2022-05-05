f6cb94d53c1a48b9fb89727dbff83ad996ce392c
Don Cross <cosinekitty@gmail.com>
2022-05-04 21:35:02 -0400

    Deleted obsolete files.

--- source/kotlin/rebuild.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM *** Helps me quickly fix syntax errors while writing new Kotlin code in Windows. ***
-pushd ..\..\generate
-bin\generate.exe source
-if errorlevel 1 (exit /b 1)
-popd
-call gradlew.bat assemble build test 
-if errorlevel 1 (exit /b 1)
-exit /b 0
