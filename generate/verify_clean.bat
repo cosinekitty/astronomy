935c4ac40369bbfdd76a36821663062391b25a7f
Don Cross <cosinekitty@gmail.com>
2022-07-21 21:43:21 -0400

    Help debug file diff in Windows build failure.

--- generate/verify_clean.bat
+++ generate/verify_clean.bat
@@ -9,6 +9,7 @@ for /f %%x in ('git status --porcelain') do (
 if !clean! == false (
     echo.verify_clean: There are local file changes - build is not clean:
     git status
+    git diff
     exit /b 1
 )
 
