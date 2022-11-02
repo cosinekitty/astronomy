1caebb60cb760ea8a6cea403abc157c9c0b1b53b
Don Cross <cosinekitty@gmail.com>
2022-11-01 20:43:28 -0400

    Doxygen download bug fix.

--- generate/commit_hook.bat
+++ generate/commit_hook.bat
@@ -9,19 +9,20 @@ REM Change to project/repo root directory.
 cd %~dp0\..
 echo.commit_hook: Repo root = %cd%
 
-echo.commit_hook: Installing Doxygen.
-md bin
-cd bin
+echo.commit_hook: Scraping Doxygen download URL.
 set DOXYGENURL=
-for /f %%x in ('py doxygen_download_link.py') do (
+for /f %%x in ('py generate/doxygen_download_link.py') do (
     set DOXYGENURL=%%x
 )
 if not defined DOXYGENURL (
     echo.commit_hook: FAIL: could not determine URL to download Doxygen binaries for Windows.
     exit /b 1
 )
+md bin
+cd bin
 echo.commit_hook: Downloading: !DOXYGENURL!
 curl -o doxygen.zip !DOXYGENURL! || exit /b 1
+echo.commit_hook: Installing Doxygen.
 7z x doxygen.zip || exit /b 1
 del doxygen.zip
 
