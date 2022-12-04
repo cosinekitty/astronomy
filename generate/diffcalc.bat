74452197944423f3eae4a6b6fd07549837a1b5a9
Don Cross <cosinekitty@gmail.com>
2022-12-04 14:52:52 -0500

    Corrected diff of nutation angles on Windows.

--- generate/diffcalc.bat
+++ generate/diffcalc.bat
@@ -20,7 +20,7 @@ if errorlevel 1 (set /a FAILCOUNT += 1)
 !CTESTEXE! diff 5.7e-15 temp\c_check.txt temp\js_check.txt
 if errorlevel 1 (set /a FAILCOUNT += 1)
 
-!CTESTEXE! diff 1.6e-16 temp\c_check.txt temp\py_check.txt
+!CTESTEXE! diff 7.11e-16 temp\c_check.txt temp\py_check.txt
 if errorlevel 1 (set /a FAILCOUNT += 1)
 
 !CTESTEXE! diff 5.7e-15 temp\js_check.txt temp\py_check.txt
