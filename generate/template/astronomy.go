b9c5bcbda2fcf406c316722260d53f4fc05d3837
Don Cross <cosinekitty@gmail.com>
2023-10-03 21:49:12 -0400

    Go: implemented AddDays function.

--- generate/template/astronomy.go
+++ generate/template/astronomy.go
@@ -1,5 +1,7 @@
 package astronomy
 
+import "math"
+
 const (
 	DaysPerTropicalYear       = 365.24217
 	SpeedOfLightAuPerDay      = 173.1446326846693
@@ -75,9 +77,178 @@ type AstroTime struct {
 	 * Historically, Terrestrial Time has also been known by the term *Ephemeris Time* (ET).
 	 */
 	Tt  float64
-	Psi float64 //For internal use only. Used to optimize Earth tilt calculations.
-	Eps float64 //For internal use only.  Used to optimize Earth tilt calculations.
-	St  float64 //For internal use only.  Lazy-caches sidereal time (Earth rotation).
+	psi float64 //For internal use only. Used to optimize Earth tilt calculations.
+	eps float64 //For internal use only.  Used to optimize Earth tilt calculations.
+	st  float64 //For internal use only.  Lazy-caches sidereal time (Earth rotation).
+}
+
+func makeTime(ut float64, tt float64) AstroTime {
+	return AstroTime{
+		Ut:  ut,
+		Tt:  tt,
+		psi: math.NaN(),
+		eps: math.NaN(),
+		st:  math.NaN(),
+	}
+}
+
+type astroDeltatFunc func(ut float64) float64
+
+var deltaTFunc astroDeltatFunc = AstronomyDeltaTEspenakMeeus
+
+func AstronomySetDeltaTFunction(funcPtr astroDeltatFunc) {
+	deltaTFunc = funcPtr
+}
+
+func terrestrialTime(ut float64) float64 {
+	return ut + deltaTFunc(ut)/86400.0
+}
+
+func universalTime(tt float64) float64 {
+	// This is the inverse function of terrestrialTime.
+	// This is an iterative numerical solver, but because
+	// the relationship between UT and TT is almost perfectly linear,
+	// it converges extremely fast (never more than 3 iterations).
+	var dt = terrestrialTime(tt) - tt
+	for {
+		var ut = tt - dt
+		var ttCheck = terrestrialTime(ut)
+		var err = ttCheck - tt
+		if math.Abs(err) < 1.0e-12 {
+			return ut
+		}
+		dt += err
+	}
+}
+
+func AstronomyDeltaTEspenakMeeus(ut float64) float64 {
+	/*
+		Fred Espenak writes about Delta-T generically here:
+		https://eclipse.gsfc.nasa.gov/SEhelp/deltaT.html
+		https://eclipse.gsfc.nasa.gov/SEhelp/deltat2004.html
+
+		He provides polynomial approximations for distant years here:
+		https://eclipse.gsfc.nasa.gov/SEhelp/deltatpoly2004.html
+
+		They start with a year value 'y' such that y=2000 corresponds
+		to the UTC Date 15-January-2000. Convert difference in days
+		to mean tropical years.
+	*/
+
+	var y, u, u2, u3, u4, u5, u6, u7 float64
+
+	y = 2000 + ((ut - 14) / DaysPerTropicalYear)
+
+	if y < -500 {
+		u = (y - 1820) / 100
+		return -20 + (32 * u * u)
+	}
+	if y < 500 {
+		u = y / 100
+		u2 = u * u
+		u3 = u * u2
+		u4 = u2 * u2
+		u5 = u2 * u3
+		u6 = u3 * u3
+		return 10583.6 - 1014.41*u + 33.78311*u2 - 5.952053*u3 - 0.1798452*u4 + 0.022174192*u5 + 0.0090316521*u6
+	}
+	if y < 1600 {
+		u = (y - 1000) / 100
+		u2 = u * u
+		u3 = u * u2
+		u4 = u2 * u2
+		u5 = u2 * u3
+		u6 = u3 * u3
+		return 1574.2 - 556.01*u + 71.23472*u2 + 0.319781*u3 - 0.8503463*u4 - 0.005050998*u5 + 0.0083572073*u6
+	}
+	if y < 1700 {
+		u = y - 1600
+		u2 = u * u
+		u3 = u * u2
+		return 120 - 0.9808*u - 0.01532*u2 + u3/7129.0
+	}
+	if y < 1800 {
+		u = y - 1700
+		u2 = u * u
+		u3 = u * u2
+		u4 = u2 * u2
+		return 8.83 + 0.1603*u - 0.0059285*u2 + 0.00013336*u3 - u4/1174000
+	}
+	if y < 1860 {
+		u = y - 1800
+		u2 = u * u
+		u3 = u * u2
+		u4 = u2 * u2
+		u5 = u2 * u3
+		u6 = u3 * u3
+		u7 = u3 * u4
+		return 13.72 - 0.332447*u + 0.0068612*u2 + 0.0041116*u3 - 0.00037436*u4 + 0.0000121272*u5 - 0.0000001699*u6 + 0.000000000875*u7
+	}
+	if y < 1900 {
+		u = y - 1860
+		u2 = u * u
+		u3 = u * u2
+		u4 = u2 * u2
+		u5 = u2 * u3
+		return 7.62 + 0.5737*u - 0.251754*u2 + 0.01680668*u3 - 0.0004473624*u4 + u5/233174
+	}
+	if y < 1920 {
+		u = y - 1900
+		u2 = u * u
+		u3 = u * u2
+		u4 = u2 * u2
+		return -2.79 + 1.494119*u - 0.0598939*u2 + 0.0061966*u3 - 0.000197*u4
+	}
+	if y < 1941 {
+		u = y - 1920
+		u2 = u * u
+		u3 = u * u2
+		return 21.20 + 0.84493*u - 0.076100*u2 + 0.0020936*u3
+	}
+	if y < 1961 {
+		u = y - 1950
+		u2 = u * u
+		u3 = u * u2
+		return 29.07 + 0.407*u - u2/233 + u3/2547
+	}
+	if y < 1986 {
+		u = y - 1975
+		u2 = u * u
+		u3 = u * u2
+		return 45.45 + 1.067*u - u2/260 - u3/718
+	}
+	if y < 2005 {
+		u = y - 2000
+		u2 = u * u
+		u3 = u * u2
+		u4 = u2 * u2
+		u5 = u2 * u3
+		return 63.86 + 0.3345*u - 0.060374*u2 + 0.0017275*u3 + 0.000651814*u4 + 0.00002373599*u5
+	}
+	if y < 2050 {
+		u = y - 2000
+		return 62.92 + 0.32217*u + 0.005589*u*u
+	}
+	if y < 2150 {
+		u = (y - 1820) / 100
+		return -20 + 32*u*u - 0.5628*(2150-y)
+	}
+
+	// all years after 2150
+	u = (y - 1820) / 100
+	return -20 + (32 * u * u)
+}
+
+func TimeFromUniversalDays(ut float64) AstroTime {
+	return makeTime(ut, terrestrialTime(ut))
+}
+
+func TimeFromTerrestrialDays(tt float64) AstroTime {
+	return makeTime(universalTime(tt), tt)
+}
+
+func (time *AstroTime) AddDays(days float64) AstroTime {
+	return TimeFromUniversalDays(time.Ut + days)
 }
 
 type CalendarDateTime struct {
@@ -272,135 +443,4 @@ type GravitySimulator struct {
 	Endpoint   [2]GravSimEndpoint
 	Prev       *GravSimEndpoint
 	Curr       *GravSimEndpoint
-}
-
-type astroDeltatFunc func(ut float64) float64
-
-var deltaTFunc astroDeltatFunc = AstronomyDeltaTEspenakMeeus
-
-func AstronomySetDeltaTFunction(funcPtr astroDeltatFunc) {
-	deltaTFunc = funcPtr
-}
-
-func TerrestrialTime(ut float64) float64 {
-	return ut + deltaTFunc(ut)/86400.0
-}
-
-func AstronomyDeltaTEspenakMeeus(ut float64) float64 {
-
-	/*
-		Fred Espenak writes about Delta-T generically here:
-		https://eclipse.gsfc.nasa.gov/SEhelp/deltaT.html
-		https://eclipse.gsfc.nasa.gov/SEhelp/deltat2004.html
-
-		He provides polynomial approximations for distant years here:
-		https://eclipse.gsfc.nasa.gov/SEhelp/deltatpoly2004.html
-
-		They start with a year value 'y' such that y=2000 corresponds
-		to the UTC Date 15-January-2000. Convert difference in days
-		to mean tropical years.
-	*/
-
-	var y, u, u2, u3, u4, u5, u6, u7 float64
-
-	y = 2000 + ((ut - 14) / DaysPerTropicalYear)
-
-	if y < -500 {
-		u = (y - 1820) / 100
-		return -20 + (32 * u * u)
-	}
-	if y < 500 {
-		u = y / 100
-		u2 = u * u
-		u3 = u * u2
-		u4 = u2 * u2
-		u5 = u2 * u3
-		u6 = u3 * u3
-		return 10583.6 - 1014.41*u + 33.78311*u2 - 5.952053*u3 - 0.1798452*u4 + 0.022174192*u5 + 0.0090316521*u6
-	}
-	if y < 1600 {
-		u = (y - 1000) / 100
-		u2 = u * u
-		u3 = u * u2
-		u4 = u2 * u2
-		u5 = u2 * u3
-		u6 = u3 * u3
-		return 1574.2 - 556.01*u + 71.23472*u2 + 0.319781*u3 - 0.8503463*u4 - 0.005050998*u5 + 0.0083572073*u6
-	}
-	if y < 1700 {
-		u = y - 1600
-		u2 = u * u
-		u3 = u * u2
-		return 120 - 0.9808*u - 0.01532*u2 + u3/7129.0
-	}
-	if y < 1800 {
-		u = y - 1700
-		u2 = u * u
-		u3 = u * u2
-		u4 = u2 * u2
-		return 8.83 + 0.1603*u - 0.0059285*u2 + 0.00013336*u3 - u4/1174000
-	}
-	if y < 1860 {
-		u = y - 1800
-		u2 = u * u
-		u3 = u * u2
-		u4 = u2 * u2
-		u5 = u2 * u3
-		u6 = u3 * u3
-		u7 = u3 * u4
-		return 13.72 - 0.332447*u + 0.0068612*u2 + 0.0041116*u3 - 0.00037436*u4 + 0.0000121272*u5 - 0.0000001699*u6 + 0.000000000875*u7
-	}
-	if y < 1900 {
-		u = y - 1860
-		u2 = u * u
-		u3 = u * u2
-		u4 = u2 * u2
-		u5 = u2 * u3
-		return 7.62 + 0.5737*u - 0.251754*u2 + 0.01680668*u3 - 0.0004473624*u4 + u5/233174
-	}
-	if y < 1920 {
-		u = y - 1900
-		u2 = u * u
-		u3 = u * u2
-		u4 = u2 * u2
-		return -2.79 + 1.494119*u - 0.0598939*u2 + 0.0061966*u3 - 0.000197*u4
-	}
-	if y < 1941 {
-		u = y - 1920
-		u2 = u * u
-		u3 = u * u2
-		return 21.20 + 0.84493*u - 0.076100*u2 + 0.0020936*u3
-	}
-	if y < 1961 {
-		u = y - 1950
-		u2 = u * u
-		u3 = u * u2
-		return 29.07 + 0.407*u - u2/233 + u3/2547
-	}
-	if y < 1986 {
-		u = y - 1975
-		u2 = u * u
-		u3 = u * u2
-		return 45.45 + 1.067*u - u2/260 - u3/718
-	}
-	if y < 2005 {
-		u = y - 2000
-		u2 = u * u
-		u3 = u * u2
-		u4 = u2 * u2
-		u5 = u2 * u3
-		return 63.86 + 0.3345*u - 0.060374*u2 + 0.0017275*u3 + 0.000651814*u4 + 0.00002373599*u5
-	}
-	if y < 2050 {
-		u = y - 2000
-		return 62.92 + 0.32217*u + 0.005589*u*u
-	}
-	if y < 2150 {
-		u = (y - 1820) / 100
-		return -20 + 32*u*u - 0.5628*(2150-y)
-	}
-
-	// all years after 2150
-	u = (y - 1820) / 100
-	return -20 + (32 * u * u)
 }
--- source/golang/astronomy_test.go
+++ source/golang/astronomy_test.go
@@ -6,10 +6,24 @@ import (
 )
 
 func TestTerrestrialTime(t *testing.T) {
-	ut := 8673.385964336088
-	tt := TerrestrialTime(ut)
-	diff := math.Abs(tt - 8673.386817342714)
-	if diff > 1.0e-16 {
-		t.Errorf("Excessive Terrestrial Time Error = %g", diff)
+	ut1 := 8673.385964336088
+	tt1 := 8673.386817342714
+	time1 := TimeFromUniversalDays(ut1)
+	diff1 := math.Abs(time1.Tt - tt1)
+	if diff1 > 1.0e-16 {
+		t.Errorf("Excessive Terrestrial Time Error = %g for time1", diff1)
+	}
+
+	ut2 := 8773.385964336088
+	tt2 := 8773.386819208281
+	time2 := time1.AddDays(100.0)
+	diff2 := math.Abs(time2.Tt - tt2)
+	if diff2 > 1.0e-16 {
+		t.Errorf("Excessive Terrestrial Time Error = %g for time2", diff2)
+	}
+
+	utdiff2 := math.Abs((time2.Ut - ut2))
+	if utdiff2 > 1.0e-16 {
+		t.Errorf("Excessive Universal Time Error = %g for time2", utdiff2)
 	}
 }
