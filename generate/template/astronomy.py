1ae6fce26633f85c88cb2623363386b5a8332bd7
Don Cross <cosinekitty@gmail.com>
2024-08-25 19:10:16 -0400

    Updated copyrights to 2024 to allow GitHub Actions tests to pass.

--- generate/template/astronomy.py
+++ generate/template/astronomy.py
@@ -2,7 +2,7 @@
 #
 #    MIT License
 #
-#    Copyright (c) 2019-2023 Don Cross <cosinekitty@gmail.com>
+#    Copyright (c) 2019-2024 Don Cross <cosinekitty@gmail.com>
 #
 #    Permission is hereby granted, free of charge, to any person obtaining a copy
 #    of this software and associated documentation files (the "Software"), to deal
